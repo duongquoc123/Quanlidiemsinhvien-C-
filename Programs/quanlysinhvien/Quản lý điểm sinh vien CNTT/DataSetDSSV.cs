a1e5bce7274f31690833983c909dbd8619ad4dd9
project-html_css <you@example.com>
2024-01-10 12:46:37 +0700

    first commit

---
+++ Programs/quanlysinhvien/Quản lý điểm sinh vien CNTT/DataSetDSSV.cs
@@ -0,0 +1,15 @@
+﻿namespace Quản_lý_điểm_sinh_vien_CNTT
+{
+
+
+    public partial class DataSetDSSV
+    {
+    }
+}
+
+namespace Quản_lý_điểm_sinh_vien_CNTT.DataSetDSSVTableAdapters {
+    
+    
+    public partial class tblSINH_VIENTableAdapter {
+    }
+}
